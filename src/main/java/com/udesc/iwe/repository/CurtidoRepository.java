ab0532e5ef566b9adfe63974346577e4fe5f50c1
gabiiwestphal <gabiwestphal@hotmail.com>
2023-10-05 11:29:42 -0300

    Commit

--- src/main/java/com/udesc/iwe/repository/CurtidoRepository.java
+++ src/main/java/com/udesc/iwe/repository/CurtidoRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.repository.query.Param;
 
 import com.udesc.iwe.models.Curtido;
 
-public interface CurtidoRepository extends JpaRepository<Curtido, Long>{
+public interface CurtidoRepository extends JpaRepository<Curtido, Long>{ // classe/entidade curtido e long é a chave primária
 
 	@Query(nativeQuery = true, value = "SELECT * FROM iwe.curtido WHERE id_usuario = :usuarioId")
-    Curtido findByUsuarioId(@Param("usuarioId") Long usuarioId);
+    Curtido findByUsuarioId(@Param("usuarioId") Long usuarioId); // especifica o tipo do valor que vai ser passado como parametro para a consulta
 	    
 	    //livros curtidos por um usuário pelo idUsuario.
 }
--- src/main/java/com/udesc/iwe/repository/UsuarioRepository.java
+++ src/main/java/com/udesc/iwe/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.udesc.iwe.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.udesc.iwe.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{ //entidade e tipo da chave primária
+	
+	Optional<Usuario> findByEmail(String email);
 
 }
