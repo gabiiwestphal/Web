1562449931f18a96225caeda2c64561194b4b6bb
gabiiwestphal <gabiwestphal@hotmail.com>
2023-10-10 17:55:29 -0300

    finalizando service

--- src/main/java/com/udesc/iwe/service/CurtidoService.java
+++ src/main/java/com/udesc/iwe/service/CurtidoService.java
@@ -1,26 +1,18 @@
 package com.udesc.iwe.service;
 
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Service;
 
 import com.udesc.iwe.models.Curtido;
-import com.udesc.iwe.models.Livro;
 import com.udesc.iwe.repository.CurtidoRepository;
-import com.udesc.iwe.repository.LivroRepository;
 
 @Service
 public class CurtidoService {
 
 	private final CurtidoRepository curtidoRepository;
-	private final LivroRepository livroRepository;
 	
-	public CurtidoService(CurtidoRepository curtidoRepository, LivroRepository livroRepository) {
+	public CurtidoService(CurtidoRepository curtidoRepository) {
 		this.curtidoRepository = curtidoRepository;
-		this.livroRepository = livroRepository;
 	}
 	
 	public Curtido listarLivrosCurtidosPorUsuario(Long idUsuario){
